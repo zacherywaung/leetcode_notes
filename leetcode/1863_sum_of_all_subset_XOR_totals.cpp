dd8371f9ef4f830787631a7b134e410b65c21d13
zacherywaung <2691205573@qq.com>
2026-01-14 10:25:17 -0800

    add leetcode1863

--- leetcode/1863_sum_of_all_subset_XOR_totals.cpp
+++ leetcode/1863_sum_of_all_subset_XOR_totals.cpp
@@ -3,43 +3,67 @@
 
 #include<vector>
 using namespace std;
+// class Solution {
+// public:
+//     int subsetXORSum(vector<int>& nums) {
+//         vector<int> path;
+//         vector<int> ret;
+//         int pos = 0;
+//         dfs(nums, path, ret, pos);
+//         int sum = 0;
+//         for(auto e : ret)
+//         {
+//             sum += e;
+//         }
+//         return sum;
+//     }
+
+//     void dfs(vector<int>& nums, vector<int>& path, vector<int>& ret, int pos)
+//     {
+//         if(pos == nums.size())
+//         {
+//             ret.push_back(calculateXOR(path));
+//             return;
+//         }
+//         //choose
+//         path.push_back(nums[pos]);
+//         dfs(nums, path, ret, pos + 1);
+//         path.pop_back();
+//         //unchoose
+//         dfs(nums, path, ret, pos + 1);
+//     }
+
+//     int calculateXOR(vector<int> path)
+//     {
+//         int ret = 0;
+//         for(int i = 0; i < path.size(); i++)
+//         {
+//             ret ^= path[i];
+//         }
+//         return ret;
+//     }
+// };
+
+
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-        vector<int> path;
-        vector<int> ret;
-        int pos = 0;
-        dfs(nums, path, ret, pos);
+        int path = 0;
         int sum = 0;
-        for(auto e : ret)
-        {
-            sum += e;
-        }
+        int pos = 0;
+        dfs(nums, path, sum, pos);
         return sum;
     }
 
-    void dfs(vector<int>& nums, vector<int>& path, vector<int>& ret, int pos)
+    void dfs(vector<int>& nums, int& path, int& sum, int pos)
     {
-        if(pos == nums.size())
+        sum += path;
+        for(int i = pos; i < nums.size(); i++)
         {
-            ret.push_back(calculateXOR(path));
-            return;
+            path ^= nums[i];
+            dfs(nums, path, sum, i + 1);
+            path ^= nums[i];
         }
-        //choose
-        path.push_back(nums[pos]);
-        dfs(nums, path, ret, pos + 1);
-        path.pop_back();
-        //unchoose
-        dfs(nums, path, ret, pos + 1);
     }
 
-    int calculateXOR(vector<int> path)
-    {
-        int ret = 0;
-        for(int i = 0; i < path.size(); i++)
-        {
-            ret ^= path[i];
-        }
-        return ret;
-    }
 };
